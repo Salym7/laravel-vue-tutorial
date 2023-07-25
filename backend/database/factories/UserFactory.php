bddc93d92edc8fdbf8ff0a917cdfc103c80282f9
Oleksandr Solianyk <salym3k@gmail.com>
2023-07-25 15:48:47 +0300

    Made backend

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -18,8 +18,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'avatar' => '/assets/avatars/avatar-' . rand(1, 7) . '.jpg',
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'company' => fake()->company(),
+            'position' => fake()->word(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
---
+++ backend/database/migrations/2023_07_25_100702_add_columns_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('position');
+            $table->string('company');
+            $table->string('lastname');
+            $table->string('firstname');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('firstname');
+            $table->dropColumn('lastname');
+            $table->dropColumn('company');
+            $table->dropColumn('position');
+        });
+    }
+};
